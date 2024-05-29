c2766758c873520109ec1fc7e574a1a9e3a81413
AsadNawaz92 <asadn7862@gmail,com>
2024-05-29 21:33:17 +0500

    Modified

--- app.js
+++ app.js
@@ -1,36 +1,74 @@
-
-var getul = document.getElementById('ul')
+let getul = document.getElementById("ul");
 
 function addtodo() {
-    var a = document.getElementById('inp')
-    var li = document.createElement('li')
-    var liText = document.createTextNode(a.value)
-    li.appendChild(liText)
-    getul.appendChild(li)
-    a.value = ''
-    var deletebtn = document.createElement('button')
-    var deletebtnText = document.createTextNode('Delete')
-    deletebtn.appendChild(deletebtnText)
-    li.appendChild(deletebtn)
-    deletebtn.setAttribute('onclick', 'del(this)')
-    var editbtn = document.createElement('button')
-    var editbtnText = document.createTextNode('Update')
-    editbtn.appendChild(editbtnText)
-    li.appendChild(editbtn)
-    editbtn.setAttribute('onclick', 'editfun(this)')
-    deletebtn.setAttribute('class', 'btn btn-danger m-2')
-    editbtn.setAttribute('class', 'btn btn-info m-2')
+  let inpEl = document.getElementById("inp");
+
+  if (inpEl.value.trim() === "") {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Input field cannot be empty!",
+    });
+    return;
+  }
+
+  let li = document.createElement("li");
+  let liText = document.createTextNode(inpEl.value);
+  li.appendChild(liText);
+  getul.appendChild(li);
+  inpEl.value = "";
+  li.classList.add(
+    "list-group-item",
+    "d-flex",
+    "justify-content-center",
+    "align-items-center"
+  );
+
+  // ******* DELETE BTN *******
+  let deletebtn = document.createElement("button");
+  let deletebtnText = document.createTextNode("Delete");
+  deletebtn.appendChild(deletebtnText);
+  li.appendChild(deletebtn);
+  deletebtn.setAttribute("onclick", "del(this)");
+
+  // ****** EDIT BTN *******
+  let editbtn = document.createElement("button");
+  let editbtnText = document.createTextNode("Update");
+  editbtn.appendChild(editbtnText);
+  li.appendChild(editbtn);
+  editbtn.setAttribute("onclick", "edit(this)");
+
+  deletebtn.setAttribute("class", "btn btn-danger m-2");
+  editbtn.setAttribute("class", "btn btn-info m-2");
 }
 
 function deleteall() {
-    getul.innerHTML = ''
+  getul.innerHTML = "";
 }
 
 function del(e) {
-    e.parentNode.remove()
+  e.parentNode.remove();
 }
 
-function editfun(e) {
-    var userEdit = prompt('Enter Edit Value', e.parentNode.firstChild.nodeValue)
-    e.parentNode.firstChild.nodeValue = userEdit
+function edit(e) {
+  let li = e.parentNode.firstChild.nodeValue;
+
+  Swal.fire({
+    title: "Update Your Task",
+    input: "text",
+    inputLabel: "Your Task",
+    inputValue: li,
+    showCancelButton: true,
+    confirmButtonText: "Update",
+    cancelButtonText: "Cancel",
+    inputValidator: (value) => {
+      if (!value) {
+        return "You need to write something!";
+      }
+    },
+  }).then((result) => {
+    if (result.isConfirmed) {
+      e.parentNode.firstChild.nodeValue = result.value;
+    }
+  });
 }
